e2c38ecd4945b28707fc911b0798501e02ee60e2
Drew Miller <dmiller@base2.io>
2018-01-16 16:42:58 -0500

    WIP: Actual and planned data

--- src/services/analyzeBurndown.js
+++ src/services/analyzeBurndown.js
@@ -1,10 +1,29 @@
-import { isBefore, isEqual, parse, startOfDay } from 'date-fns';
-import { find, isEmpty, negate, overSome, sortBy, sumBy } from 'lodash';
+import {
+  eachDay,
+  isBefore,
+  isEqual,
+  isWeekend,
+  parse,
+  startOfDay,
+} from 'date-fns';
+import {
+  difference,
+  find,
+  isEmpty,
+  last,
+  negate,
+  overSome,
+  rearg,
+  sortBy,
+  sumBy,
+} from 'lodash';
 
 const alignToDay = f => (...dates) => f(...dates.map(startOfDay));
 const isBeforeDay = alignToDay(isBefore);
+const isAfterDay = rearg(isBeforeDay, [1, 0]);
 const isEqualDay = alignToDay(isEqual);
 const isBeforeOrEqualDay = overSome(isBeforeDay, isEqualDay);
+const isWeekday = negate(isWeekend);
 
 const hasOverlaps = sprints =>
   sortBy(sprints, 'startsAt').some((sprint, i) =>
@@ -22,23 +41,44 @@ const hasVelocity = (stories, asOf) =>
     story => story.closedAt && isBeforeOrEqualDay(story.closedAt, asOf),
   );
 
+const isCompleted = (sprint, asOf) => isAfterDay(asOf, sprint.endsAt);
+
+const workingDays = (...sprints) =>
+  sprints.reduce(
+    (sum, sprint) =>
+      (sum += eachDay(sprint.startsAt, sprint.endsAt).filter(isWeekday).length),
+    0,
+  );
+
 const resolveError = error => ({ error, data: null });
 
-const resolveData = (sprint, stories, asOf, dateKey = 'endsAt') => {
+const resolveActualData = (sprint, stories, asOf, dateKey = 'endsAt') => {
   const date = parse(sprint[dateKey]);
-  const type = isBeforeDay(date, asOf) ? 'actual' : 'planned';
   const totalStories = stories.filter(story =>
     isBeforeOrEqualDay(story.openedAt, sprint.endsAt),
   );
+  const total = sumBy(totalStories, 'estimate');
   const openStories = totalStories.filter(
     story => !story.closedAt || isBeforeDay(date, story.closedAt),
   );
+  const open = sumBy(openStories, 'estimate');
+  const closed = total - open;
+
+  return { closed, date, open, total, type: 'actual' };
+};
+
+const resolvePlannedData = (sprint, i, lastActual, velocity) => {
+  const plannedWorkedDays = workingDays(sprint);
+  const closing = Math.floor(plannedWorkedDays * velocity * (i + 1));
+  const closed = Math.min(lastActual.closed + closing, lastActual.total);
+  const open = Math.max(lastActual.open - closing, 0);
 
   return {
-    date,
-    type,
-    open: sumBy(openStories, 'estimate'),
-    total: sumBy(totalStories, 'estimate'),
+    closed,
+    open,
+    date: parse(sprint.endsAt),
+    total: lastActual.total,
+    type: 'planned',
   };
 };
 
@@ -48,9 +88,30 @@ function analyzeBurndown(sprints, stories, asOf) {
   if (isEmpty(stories)) return resolveError('No stories.');
   if (!hasVelocity(stories, asOf)) return resolveError('No velocity.');
 
-  const data = [resolveData(sprints[0], stories, asOf, 'startsAt')].concat(
-    sprints.map(sprint => resolveData(sprint, stories, asOf)),
+  // Actuals:
+
+  const completedSprints = sprints.filter(sprint => isCompleted(sprint, asOf));
+  const actualData = [
+    resolveActualData(completedSprints[0], stories, asOf, 'startsAt'),
+  ].concat(
+    completedSprints.map(sprint => resolveActualData(sprint, stories, asOf)),
   );
+  const lastActual = last(actualData);
+  const actualWorkedDays = workingDays(...completedSprints);
+  const actualVelocity = lastActual.closed / actualWorkedDays;
+
+  // Planned:
+
+  const plannedSprints = difference(sprints, completedSprints);
+  const plannedData = plannedSprints.map((sprint, i) =>
+    resolvePlannedData(sprint, i, lastActual, actualVelocity),
+  );
+
+  // Projected:
+  // TODO: Analyze projected data.
+  const projectedData = [];
+
+  const data = sortBy(actualData.concat(plannedData, projectedData), 'date');
 
   return {
     data,
--- src/services/analyzeBurndown.spec.js
+++ src/services/analyzeBurndown.spec.js
@@ -17,6 +17,7 @@ test('flags an error when no stories are sent', () => {
       startsAt: format('2018-01-01'),
     },
   ];
+
   const analysis = analyzeBurndown(sprints, [], format('2018-01-16'));
 
   expect(analysis).toEqual({ data: null, error: 'No stories.' });
@@ -42,6 +43,7 @@ test('flags an error when no stories have been closed', () => {
       title: '#1: Open Issue',
     },
   ];
+
   const analysis = analyzeBurndown(sprints, stories, format('2018-01-16'));
 
   expect(analysis).toEqual({ data: null, error: 'No velocity.' });
@@ -80,7 +82,39 @@ test('flags an error when sprints overlap', () => {
   expect(analysis).toEqual({ data: null, error: 'Overlapping sprints.' });
 });
 
-test('analyzes a project', () => {
+test('analyzes a completed on-target project', () => {
+  const sprints = [
+    {
+      endsAt: format('2018-01-12'),
+      id: 1,
+      name: 'Sprint 1',
+      number: 1,
+      startsAt: format('2018-01-01'),
+    },
+  ];
+  const stories = [
+    {
+      closedAt: format('2018-01-05'),
+      estimate: 5,
+      id: 1,
+      number: 1,
+      openedAt: format('2017-12-29'),
+      title: '#1: Closed',
+    },
+  ];
+
+  const analysis = analyzeBurndown(sprints, stories, format('2018-01-16'));
+
+  expect(analysis).toEqual({
+    data: [
+      { closed: 0, date: parse('2018-01-01'), open: 5, total: 5, type: 'actual' },
+      { closed: 5, date: parse('2018-01-12'), open: 0, total: 5, type: 'actual' },
+    ],
+    error: null,
+  });
+});
+
+test('analyzes a planned on-target project', () => {
   const sprints = [
     {
       endsAt: format('2018-01-12'),
@@ -96,33 +130,47 @@ test('analyzes a project', () => {
       number: 2,
       startsAt: format('2018-01-15'),
     },
+    {
+      endsAt: format('2018-02-09'),
+      id: 3,
+      name: 'Sprint 3',
+      number: 3,
+      startsAt: format('2018-01-29'),
+    },
   ];
   const stories = [
     {
-      closedAt: null,
-      estimate: 2,
+      closedAt: format('2018-01-05'),
+      estimate: 5,
       id: 1,
       number: 1,
       openedAt: format('2017-12-29'),
-      title: '#1: Open Issue',
+      title: '#1: Closed',
     },
     {
-      closedAt: format('2018-01-05'),
-      estimate: 5,
+      closedAt: null,
+      estimate: 2,
       id: 2,
       number: 2,
       openedAt: format('2017-12-29'),
-      title: '#2: Closed Issue',
+      title: '#2: Open',
     },
   ];
+
   const analysis = analyzeBurndown(sprints, stories, format('2018-01-16'));
 
   expect(analysis).toEqual({
     data: [
-      { date: parse('2018-01-01'), open: 7, total: 7, type: 'actual' },
-      { date: parse('2018-01-12'), open: 2, total: 7, type: 'actual' },
-      { date: parse('2018-01-26'), open: 2, total: 7, type: 'planned' },
+      { closed: 0, date: parse('2018-01-01'), open: 7, total: 7, type: 'actual' },
+      { closed: 5, date: parse('2018-01-12'), open: 2, total: 7, type: 'actual' },
+      { closed: 7, date: parse('2018-01-26'), open: 0, total: 7, type: 'planned' },
+      { closed: 7, date: parse('2018-02-09'), open: 0, total: 7, type: 'planned' },
     ],
     error: null,
-  });
+  })
 });
+
+test('analyzes an actual off-target project');
+test('analyzes a planned off-target project');
+test('handle stories closed before the first sprint');
+test('handle stories that close in the gap between sprints');
