806f962e50e7f9bba5c99a19e84320d0f1d9864b
Kim Hamper <khamper+netjets@base2.io>
2018-02-07 09:00:52 -0500

    WIP: Add velocity and story sprint to report

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,23 @@
+import { isWithinRange } from 'date-fns';
+import { find } from 'lodash';
+
+const getVelocity = (stories, sprint) =>
+  stories.reduce((result, story) => {
+    if (isWithinRange(story.closedAt, sprint.startsAt, sprint.endsAt)) {
+      return result + story.estimate;
+    }
+
+    return result;
+  }, 0);
+
+const getSprint = (sprints, story) => {
+  const storySprint = find(sprints, sprint =>
+    isWithinRange(story.closedAt, sprint.startsAt, sprint.endsAt),
+  );
+
+  return storySprint ? storySprint.number : 'None';
+};
+
 const activeStepSelector = state =>
   Math.max(state.ui.steps.findIndex(s => s.active), 0);
 
@@ -18,8 +38,14 @@ const projectSelector = id => state =>
   state.entities.projects.find(p => p.id === id);
 
 const reportSelector = state => ({
-  sprints: state.entities.sprints,
-  stories: state.entities.stories,
+  sprints: state.entities.sprints.map(sprint => ({
+    ...sprint,
+    velocity: getVelocity(state.entities.stories, sprint),
+  })),
+  stories: state.entities.stories.map(story => ({
+    ...story,
+    sprint: getSprint(state.entities.sprints, story),
+  })),
   timeEntries: state.entities.timeEntries,
 });
 
